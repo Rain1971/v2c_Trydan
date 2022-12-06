2fdd72051d90bf3b77f51825d85a5c614d507961
“Rain1971” <rafa@tarracoimportexport.com>
2022-12-06 14:47:31 +0100

    Estabilizadas la lectornas en tiempo para evitar errores

--- app.py
+++ app.py
@@ -10,24 +10,31 @@ CONTROLV2C_PORT     = 5002
 CONTROLV2C_DEBUG    = True
 # ------------------------------------------------------------------------
 
-
-datosV2C = LecturaDatos( IP_V2C )
+datosV2C = None
 
 @app.route('/estado')
 def estado():
-  datos   = datosV2C.valores()
-  cadena  = ""
-  for parametro in list( datos ):
-    cadena += '<p> {}: {} </p>'.format( parametro, datos[parametro] )
-  return cadena
+  global datosV2C
+  if datosV2C is not None:
+    datos   = datosV2C.valores()
+    cadena  = ""
+    for parametro in list( datos ):
+      cadena += '<p> {}: {} </p>'.format( parametro, datos[parametro] )
+    return cadena
+  else:
+    datosV2C = LecturaDatos( IP_V2C )
+    return "No inicializado, refresca la página en unos segundos"
 
 @app.route('/')
 def inicio():
-    return 'Control de estado para cargador V2C!'
+  global datosV2C 
+  if datosV2C is None:
+    datosV2C = LecturaDatos( IP_V2C )
+  return 'Control de estado para cargador V2C!'
+
 
 if __name__ == "__main__":
-
   app.run( host = CONTROLV2C_HOSTNAME, 
            port = CONTROLV2C_PORT, 
-           threaded = False,
+           threaded = True,
            debug = CONTROLV2C_DEBUG )
--- hilo_lecturas.py
+++ hilo_lecturas.py
@@ -4,6 +4,7 @@ from loguru import logger
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.client import ModbusTcpClient
+from pymodbus.constants import Defaults
 
 # --------------------------------------------------------------------------------
 
@@ -15,6 +16,7 @@ class LecturaDatos(threading.Thread):
     self.isRunning = True
     self.daemon = True
     self.ip = ip
+    Defaults.Timeout = 10
     logger.add("log/hilo_lecturas.log",
                 format="{time} - {level} - {function}:{line} - {message}", rotation="100 MB")
     logger.info("Hilo iniciado ")
@@ -50,29 +52,31 @@ class LecturaDatos(threading.Thread):
 
   def run(self):
     try:
-      cliente = ModbusTcpClient( host=self.ip, port=502 )
-      connection = cliente.connect()
-      time.sleep(2)
-
-      while self.isRunning:
-        time.sleep(10)
-        try:
-          if connection:
-            distancia = len( self.datos ) * 2
-            valor = cliente.read_holding_registers( 0x0BC2, distancia, unit=1 )
-            lista = self.divide( valor.registers, 2 )
-            posicion = 0
-            for direccion in list( self.datos ):
-              valor.registers = lista[posicion]
-              self.datos[direccion] = self.regeneraFloat( valor )
-              posicion += 1
-          else:
-            logger.error("Conexion perdida con V2C ")
-            cliente = ModbusTcpClient( host=self.ip, port=502 )
-            connection = cliente.connect()
-        except Exception as e:
-            logger.error("Error en captura de datos: ")
-            logger.error(e)
+      with ModbusTcpClient( host=self.ip, port=502, timeout=10 ) as cliente:
+        connection = cliente.connect()
+        time.sleep(2)
+        while self.isRunning:       
+          try:
+            if connection:
+              distancia = len( self.datos ) * 2
+              valor = cliente.read_holding_registers( 0x0BC2, distancia, unit=1 )
+              time.sleep(2)
+              if len(valor.registers) == distancia:
+                lista = self.divide( valor.registers, 2 )
+                posicion = 0
+                for direccion in list( self.datos ):
+                  valor.registers = lista[posicion]
+                  self.datos[direccion] = self.regeneraFloat( valor )
+                  posicion += 1
+              else:
+                logger.error("No hay recepcion de datos ")  
+            else:
+              logger.error("Conexion perdida con V2C ")
+              cliente = ModbusTcpClient( host=self.ip, port=502 )
+              connection = cliente.connect()
+          except Exception as e:
+              logger.error("Error en captura de datos: ")
+              logger.error(e)
     except Exception as e:
       logger.error("Fallo en la conexion: ")
       logger.error(e)
